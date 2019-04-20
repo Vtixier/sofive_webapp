bf872114c0e394ef11f6f11d2b3889ffc6433611
xavier <xavier.green@student.ecp.fr>
2019-04-20 19:00:33 +0200

    Footer component

---
+++ public/js/footer.js
@@ -0,0 +1,12 @@
+Vue.component("sofive-footer", {
+  template: `
+		<div class="footer">
+            <div class="float-right">
+                Powered by ScorePlay
+            </div>
+            <div>
+                <strong> <img src="https://sofive.com/wp-content/uploads/2018/04/cropped-sigle-32x32.png" style="width: 20px" /> Sofive Soccer Center</strong>
+            </div>
+        </div>`,
+  props: []
+});
