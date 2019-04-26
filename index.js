6ff1dfc84ccbca4f0ae73f24c44debcbc383e37f
xavier <xavier.green@student.ecp.fr>
2019-04-26 12:15:09 -0400

    https test

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const compression = require('compression');
 const app = express();
+const https = require('https');
+const fs = require('fs');
 
 app.use(express.static('public', { maxAge: 86400000 }));
 app.use(compression());
@@ -9,4 +11,7 @@ app.use('/', express.static('public'));
 
 const port = process.env.PORT || 80
 
-app.listen(port, () => console.log('Website server listening on port '+port+'!'))
+https.createServer({
+  key: fs.readFileSync('./https/sofive.com2018.key'),
+  cert: fs.readFileSync('./https/sofive.com2018.crt')
+}, app).listen(port, () => console.log('Website server listening on port '+port+'!'))
