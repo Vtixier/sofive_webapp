ad5d93b998a09fb3b6e41fd35f9a684d75e37b39
xavier <xavier.green@student.ecp.fr>
2019-04-26 12:20:18 -0400

    Https fix

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const compression = require('compression');
 const app = express();
+const http = require('http');
 const https = require('https');
 const fs = require('fs');
+
+const https_credentials = {
+  key: fs.readFileSync('./https/sofive.com2018.key', 'utf8'),
+  cert: fs.readFileSync('./https/sofive.com2018.crt', 'utf8')
+}
 
 app.use(express.static('public', { maxAge: 86400000 }));
 app.use(compression());
 
 app.use('/', express.static('public'));
 
-const port = process.env.PORT || 80
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(https_credentials, app);
 
-https.createServer({
-  key: fs.readFileSync('./https/sofive.com2018.key'),
-  cert: fs.readFileSync('./https/sofive.com2018.crt')
-}, app).listen(port, () => console.log('Website server listening on port '+port+'!'))
+httpServer.listen(80, () => console.log('Http server up!'));
+httpsServer.listen(443, () => console.log('Https server up!'));
