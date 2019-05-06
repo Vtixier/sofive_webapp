4c4ebb348e5ee90cfa8bd0ca67f124939efd1828
xavier <xavier.green@student.ecp.fr>
2019-05-05 23:48:57 -0400

    Scoreplay branding

--- public/js/footer.js
+++ public/js/footer.js
@@ -2,7 +2,7 @@ Vue.component("sofive-footer", {
   template: `
 		<div class="footer">
             <div class="float-right">
-                Powered by ScorePlay
+                Powered by <a href="https://scoreplay.io">ScorePlay</a>
             </div>
             <div>
                 <strong> <img src="https://sofive.com/wp-content/uploads/2018/04/cropped-sigle-32x32.png" style="width: 20px" /> Sofive Soccer Center</strong>
