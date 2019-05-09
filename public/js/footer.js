42a97d61c434c27b2a8058949a0c2bcd4c448261
xavier <xavier.green@student.ecp.fr>
2019-05-09 18:28:10 +0200

    Minor fixes

--- public/js/footer.js
+++ public/js/footer.js
@@ -1,5 +1,6 @@
 Vue.component("sofive-footer", {
   template: `
+  <div class="marge">
 		<div class="footer">
             <div class="float-right">
                 Powered by <a href="https://scoreplay.io">ScorePlay</a>
@@ -7,6 +8,7 @@ Vue.component("sofive-footer", {
             <div>
                 <strong> <img src="https://sofive.com/wp-content/uploads/2018/04/cropped-sigle-32x32.png" style="width: 20px" /> Sofive Soccer Center</strong>
             </div>
-        </div>`,
+        </div>
+  </div>`,
   props: []
 });
