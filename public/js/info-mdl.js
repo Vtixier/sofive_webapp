1c23b823266dace69d3175d54356116dd0789afa
xavier <xavier.green@student.ecp.fr>
2019-06-04 14:18:27 +0000

    Center info

---
+++ public/js/info-mdl.js
@@ -0,0 +1,73 @@
+Vue.component('sofive-mdl-info', {
+	template: `
+	<div class="wpb_wrapper">
+            <h3><i class="fa fa-phone"></i> +43 100 783 001</h3>
+        <span class="medium">
+            Please contact with us if you have any questions.
+        </span><br/><br/>
+            <div class="wpb_text_column wpb_content_element ">
+                <div class="wpb_wrapper">
+                    <h3>General info</h3>
+                </div>
+            </div>
+            <div class="vc_empty_space" style="height: 8px">
+                <span class="vc_empty_space_inner">
+                    <span class="empty_space_image"></span>
+                </span></div>
+            <div class="wpb_text_column wpb_content_element ">
+                <div class="wpb_wrapper">
+                    <p style="text-align: left;">Address: <a style="text-decoration: underline;">2 Palmer Terrace, Carlstadt, NJ 07072</a><br>
+                        Email: meadowlands@sofive.com<br>
+                        Open weekdays from 9:00 am to 1:00 am and weekends 7:00 am to 1:00 am</p>
+                </div>
+            </div>
+            <div class="wpb_text_column wpb_content_element ">
+                <div class="wpb_wrapper">
+                    <h3>Amenities</h3>
+                </div>
+            </div>
+
+
+            <div class="wpb_text_column wpb_content_element" style="text-align: left">
+                <div class="wpb_wrapper">
+                    <p>
+                        <span data-type="normal" class="qode_icon_shortcode q_font_awsome_icon fa-lg  ">
+                            <i class="qode_icon_font_awesome fa fa-thermometer-half qode_icon_element" style="color: #04b2e2;"></i>
+                        </span>&nbsp; &nbsp; Climate controlled lounge and field house
+                    </p>
+                    <p>
+                        <span data-type="normal" class="qode_icon_shortcode q_font_awsome_icon fa-lg  ">
+                            <i class="qode_icon_font_awesome fa fa-television qode_icon_element" style="color: #04b2e2;"></i>
+                        </span>&nbsp; &nbsp; All Live Games on TVs
+                    </p>
+                    <p>
+                        <span data-type="normal" class="qode_icon_shortcode  q_font_awsome_icon fa-lg  " style=" ">
+                            <i class="qode_icon_font_awesome fa fa-beer qode_icon_element" style="color: #04b2e2;"></i>
+                        </span>&nbsp; &nbsp; Bar &amp; Café (incl. beer)
+                    </p>
+                    <p>
+                        <span data-type="normal" class="qode_icon_shortcode  q_font_awsome_icon fa-lg  " style=" ">
+                            <i class="qode_icon_font_awesome fa fa-futbol-o qode_icon_element" style="color: #04b2e2;"></i>
+                        </span>&nbsp; &nbsp; 8 indoor fields
+                    </p>
+                    <p>
+                        <span data-type="normal" class="qode_icon_shortcode  q_font_awsome_icon fa-lg  " style=" ">
+                            <i class="qode_icon_font_awesome fa fa-video-camera qode_icon_element" style="color: #04b2e2;"></i>
+                        </span>&nbsp; &nbsp; 8 fields equipped with video cameras & scoreboards
+                    </p>
+                    <p>
+                        <span data-type="normal" class="qode_icon_shortcode  q_font_awsome_icon fa-lg  " style=" ">
+                            <i class="qode_icon_font_awesome fa fa-car qode_icon_element" style="color: #04b2e2;"></i>
+                        </span>&nbsp; &nbsp; Free on-site parking
+                    </p>
+                    <p>
+                        <span data-type="normal" class="qode_icon_shortcode  q_font_awsome_icon fa-lg  " style=" ">
+                            <i class="qode_icon_font_awesome fa fa-wifi qode_icon_element" style="color: #04b2e2;"></i>
+                        </span>&nbsp; &nbsp; Free wifi
+                    </p>
+
+                </div>
+            </div>
+        </div>
+        `
+})
