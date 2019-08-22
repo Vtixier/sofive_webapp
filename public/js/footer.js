405929df9ba63f0899d1a8316c178a4d89d33611
xavier <xavier.green@student.ecp.fr>
2019-08-22 12:26:00 +0200

    fix external components

--- public/js/footer.js
+++ public/js/footer.js
@@ -1,11 +1,3 @@
 Vue.component("sofive-footer", {
-  template: `
-  <div class="marge">
-		<div class="footer">
-            <div class="float-right">
-                <strong> Powered by : <a href="https://scoreplay.io"> <img src="img/sofive/logo_vert.png" style="width: 115px; margin-bottom: 2%;"/></strong></a>
-            </div>
-        </div>
-  </div>`,
-  props: []
+  template: "<div class=\"marge\"><div class=\"footer\"><div class=\"float-right\"><strong> Powered by : <a href=\"https://scoreplay.io\"> <img src=\"img/sofive/logo_vert.png\" style=\"width: 115px; margin-bottom: 2%;\"/></strong></a></div></div></div>",
 });
