d75da6655f95d1ad317fca9e462eb73b98e6ecb6
xavier <xavier.green@student.ecp.fr>
2020-02-28 19:55:15 +0000

    Goal of the month

--- public/js/footer.js
+++ public/js/footer.js
@@ -1,3 +1,3 @@
 Vue.component("sofive-footer", {
-  template: "<div class=\"marge\"><div class=\"footer\"><div class=\"float-left\"><a href=\"/privacy.html\">Privacy</a></div><div class=\"float-right\"><strong> Powered by : <a href=\"https://scoreplay.io\"> <img src=\"/img/sofive/logo_vert.png\" style=\"width: 115px; margin-bottom: 2%;\"/></strong></a></div></div></div>",
+  template: "<div class=\"marge\"><div class=\"footer\"><div class=\"float-left\"><a href=\"/privacy.html\">Privacy</a></div><div class=\"float-right\"><strong> Powered by : <a href=\"https://scoreplay.io/index_en.html\"> <img src=\"/img/sofive/logo_vert.png\" style=\"width: 115px; margin-bottom: 2%;\"/></strong></a></div></div></div>",
 });
